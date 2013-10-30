a61bc759b1939f2118fb864b819b23e2f056c869
Liam Craver <studios.lime@gmail.com>
2013-10-30 00:54:39 -0500

    Added Email / Update functions to User class

--- src/app/classes/User.php
+++ src/app/classes/User.php
@@ -9,8 +9,18 @@
 
 		public function create($fields = array()) {
 			if(!$this->_db->insert('users', $fields)) {
-				throw new Exception('There was a problem creating an account.');
+				throw new Exception('There was a problem creating your account.');
 			}
+		}
+
+		public function update($id, $fields = array()) {
+			if(!$this->_db->update('users', $id, $fields)) {
+				throw new Exception('There was a problem updating your account.');
+			}
+		}
+
+		public function email($to, $subject, $body) {
+			mail($to, $subject, $body, "From: admin@limeade.net");
 		}
 	}
 
