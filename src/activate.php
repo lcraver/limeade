cbb1f94cea4ff3dd90eed08569ff5df68f421641
Liam Craver <studios.lime@gmail.com>
2013-10-30 00:55:27 -0500

    Added the activation page for account activation via email

---
+++ src/activate.php
@@ -0,0 +1,33 @@
+<?php
+
+	require_once 'app/core/Init.php';
+
+	if(isset($_GET['email'], $_GET['email_code']) === true) {
+		$user = new User();
+
+		$email = 		trim($_GET['email']);
+		$email_code = 	trim($_GET['email_code']);
+
+		try {
+
+			$emailCheck = DB::getInstance()->get('users', array('email', '=', $email))->first()->id;
+			$emailCodeCheck = DB::getInstance()->get('users', array('email_code', '=', $email_code))->first()->id;
+
+			if($emailCheck == $emailCodeCheck) {
+				$user->update($emailCheck, array(
+					'active' => 1
+				));
+
+				Session::flash('home', 'Your account has been activated and you can now login!');
+				header('Location: index.php');
+			} else {
+				Session::flash('home', 'Your account could not be activated!');
+				header('Location: index.php');
+			}
+
+		} catch(Exception $e) {
+			die($e->getMessage());
+		}
+	}
+
+?>
