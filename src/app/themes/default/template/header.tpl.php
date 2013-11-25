3abeecf0d42b6cffa0f9fa70268d0ec783c1a8bf
Liam Craver <studios.lime@gmail.com>
2013-11-24 18:56:08 -0600

    Added to header to make theme work on mobile.

--- src/app/themes/default/template/header.tpl.php
+++ src/app/themes/default/template/header.tpl.php
@@ -1,3 +1,5 @@
+<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1"/>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <?php
 
 # Load all of the selected theme's CSS files
